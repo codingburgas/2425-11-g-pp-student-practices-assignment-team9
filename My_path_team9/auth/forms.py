2387b416a704fec44d20205e354f68425a591e6f
Viktoria <VGKupenova21@codingburgas.bg>
2025-05-23 22:51:46 +0300

    Create the login and register pages

---
+++ My_path_team9/auth/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import SelectField, StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import Email
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=3)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    role = SelectField("Role", choices=[('student', 'Student'), ('teacher', 'Teacher')], validators=[DataRequired()])
+    submit = SubmitField("Register")
+
---
+++ My_path_team9/auth/models.py
@@ -0,0 +1,32 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from .. import db
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    __password = db.Column("password", db.String(128), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        if not self._is_valid_password(password):
+            raise ValueError("Password must be at least 8 characters long and contain one uppercase letter, one lowercase letter, and one digit.")
+        self.__password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.__password, password)
+
+    def _is_valid_password(self, password):
+        import re
+
+        return (len(password) >= 8 and
+                re.search(r'[A-Z]', password) and
+                re.search(r'[a-z]', password) and
+                re.search(r'\d', password))
