c18a7258f16f2c454418cee2957b76802810cd81
amzheleva21 <amzheleva21@codingburgas.bg>
2025-06-10 13:46:55 +0300

    Create teacher modul

--- My_path_team9/__init__.py
+++ My_path_team9/__init__.py
@@ -17,9 +17,11 @@ def create_app(config):
     from .auth import auth_bp
     from .main import main_bp
     from .student import student_bp
+    from .teacher import teacher_bp
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
     app.register_blueprint(student_bp)
+    app.register_blueprint(teacher_bp)
 
     return app
