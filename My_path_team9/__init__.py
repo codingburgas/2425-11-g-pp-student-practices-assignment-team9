92d746a27f413b1cf282dcdc9efee9c19cf221eb
amzheleva21 <amzheleva21@codingburgas.bg>
2025-06-16 13:02:14 +0300

    Register errors.py and update it

--- My_path_team9/__init__.py
+++ My_path_team9/__init__.py
@@ -3,9 +3,11 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 bootstrap = Bootstrap()
 login_manager = LoginManager()
+
 
 def create_app(config):
     app = Flask(__name__)
@@ -18,10 +20,12 @@ def create_app(config):
     from .main import main_bp
     from .student import student_bp
     from .teacher import teacher_bp
+    from .errors import register_error_handlers
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
     app.register_blueprint(student_bp)
     app.register_blueprint(teacher_bp)
+    register_error_handlers(app)
 
     return app
